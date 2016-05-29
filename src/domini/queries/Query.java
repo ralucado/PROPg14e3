5448b43cb8e63012baf8cd0a15f9c59b7bf4e643
marc-villanueva-cuadrench <marcvillanuevacuadrench@gmail.com>
2016-05-29 23:50:12 +0200

    Update Query.java

--- src/domini/queries/Query.java
+++ src/domini/queries/Query.java
@@ -1,26 +1,48 @@
 package domini.queries;
 
 import domini.camins.Cami;
-
+/**
+ * La classe Query és una classe abstracta que té com a subclasses QueryNormal i QueryClustering
+ * @author Marc Villanueva Cuadrench
+ *
+ */
 public abstract class Query {
 	protected Cami cami;
 	
+	/**
+	 * Creadora que guarda un Camí 
+	 * @param c Camí a guardar
+	 */
 	public Query(Cami c) {
 		this.cami = c;
 	}
 	
-	protected Query() throws Exception {
+	/**
+	 * Creadora per defecte
+	 * @throws Exception
+	 */
+	protected Query(){
 		
 	}
 	
+	/**
+	 * Consultora del camí de la query
+	 * @return String amb el camí de la query
+	 */
 	public String getPath() {
 		return cami.getPath();
 	}
 
-	
-	
+	/**
+	 * Mètode abstracte per saber si la query és de tipus clustering
+	 * @return Depen de la subclasse
+	 */
 	public abstract boolean esClustering();
 	
+	/**
+	 * Mètode abstracte per saber si la query és de tipus normal
+	 * @return Depen de la subclasse
+	 */
 	public abstract boolean esNormal();
 }
 
