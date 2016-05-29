85bda72e521e2876c5152bd8aa21623d0eef636b
marc-villanueva-cuadrench <marcvillanuevacuadrench@gmail.com>
2016-05-29 16:02:44 +0200

    Update VistaDetallsRelacio.java

--- src/presentacio/VistaDetallsRelacio.java
+++ src/presentacio/VistaDetallsRelacio.java
@@ -1,30 +1,9 @@
-package presentacio;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Point;
-import java.awt.SystemColor;
-import java.awt.TextField;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import domini.graf.Label;
-import net.miginfocom.swing.MigLayout;
-
-import javax.swing.JComboBox;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.ListModel;
+import javax.swing.UIManager;
 
 public class VistaDetallsRelacio extends JDialog {
 
@@ -36,32 +15,38 @@ public class VistaDetallsRelacio extends JDialog {
 
 
 	private void initComponents(){
+		
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		}
+		catch(Exception e){}
+		
 		//frame
 		
 		setBackground(SystemColor.control);
 		setForeground(Color.BLACK);
-		setSize(800, 200);
+		setSize(800, 150);
 		setLocationRelativeTo(null);
 		
-		getContentPane().setLayout(new MigLayout("", "[250px,grow][250px,grow]", "[50px,grow][50px,grow][50px,grow][50px,grow][50px,grow][50px,grow]"));
+		getContentPane().setLayout(new MigLayout("", "[800px,grow]", "[30px,grow][30px,grow][30px,grow][30px,grow][30px,grow][30px,grow][30px, grow]"));
 		
-		JLabel lblLabel = new JLabel("El nom de l'entitat 1 és:  "+nomE1);
+		//label nom1
+		JLabel lblLabel = new JLabel("<html><b>El nom de l'entitat 1 és:</b>  "+nomE1+"</html>");
 		lblLabel.setBounds(255, 10, 136, 15);
 		getContentPane().add(lblLabel, "cell 0 0,alignx left,aligny center");
-		
-		
-		//label nom
-		JLabel lblNomDeLentitat = new JLabel("El nom de l'entitat 2 és:  "+nomE2);
+
+		//label nom2
+		JLabel lblNomDeLentitat = new JLabel("<html><b>El nom de l'entitat 2 és:  </b>"+nomE2+"</html>");
 		lblNomDeLentitat.setBounds(30, 71, 136, 15);
 		getContentPane().add(lblNomDeLentitat, "cell 0 4,alignx left,aligny center");
 		
-		//label tipus
-		JLabel lblTipusDentitat = new JLabel("L'entitat 1 és del tipus:  "+tipusE1);
+		//label tipus1
+		JLabel lblTipusDentitat = new JLabel("<html><b>L'entitat 1 és del tipus:  </b>"+tipusE1+"</html>");
 		lblTipusDentitat.setBounds(30, 10, 136, 15);
 		getContentPane().add(lblTipusDentitat, "cell 0 2,alignx left,aligny center");
 		
-		//label id
-		JLabel lblId = new JLabel("L'entitat 2 és del tipus:  "+tipusE2);
+		//label nom2
+		JLabel lblId = new JLabel("<html><b>L'entitat 2 és del tipus:  </b>"+tipusE2+"</html>");
 		getContentPane().add(lblId, "cell 0 6,alignx left, aligny center");
 		
 		//boto cancelar
@@ -71,7 +56,7 @@ public class VistaDetallsRelacio extends JDialog {
 				dispose();
 			}
 		});
-		getContentPane().add(cancelButton, "cell 1 8,alignx left,aligny center");
+		getContentPane().add(cancelButton, "cell 0 7,alignx left,aligny center");
 	}
 	
 	
@@ -88,24 +73,9 @@ public class VistaDetallsRelacio extends JDialog {
 		this.tipusE2 = tipus2;
 		this.ctrl = ctrl;
 		initComponents();
-		int l = maxLength();
-		this.setSize(330+7*l, 200);
+		this.setSize(500, 300);
 		setVisible(true);
 		setLocationRelativeTo(owner);
 	}
-	
-	private int maxLength(){
-		int l1 = nomE1.length();
-		int l2 = nomE2.length();
-		int l3 = tipusE1.length();
-		int l4 = tipusE2.length();
-		int la, lb;
-		if(l1>l2) la = l1;
-		else la = l2;
-		if(l3>l4) lb = l3;
-		else lb = l4;
-		
-		if(la>lb) return la;
-		else return lb;
-	}
+
 }
