b45d64e777ca7881e40f80c9755bd970c0f81572
marc-villanueva-cuadrench <marcvillanuevacuadrench@gmail.com>
2016-05-29 15:22:22 +0200

    Update ControladorQueries.java

--- src/domini/queries/ControladorQueries.java
+++ src/domini/queries/ControladorQueries.java
@@ -5,6 +5,11 @@ import domini.camins.Cami;
 import domini.graf.CtrlGraf;
 import domini.graf.Entitat;
 
+/**
+ * La classe ControladorQueries és el controlador que s'encarrega de realitzar les cerques (queries) i gestionar els resultats
+ * @author Marc Villanueva Cuadrench
+ *
+ */
 public class ControladorQueries{
 	private ArrayList<Query> UltimesQueries;
 	private Query QueryActual;
@@ -12,24 +17,44 @@ public class ControladorQueries{
 	private CtrlGraf CtrlGraf;
 	private HeteSim HS;
 	
+	/**
+	 * Creadora de ControladorQueries
+	 * @param CtrlGraf Controlador del graf
+	 */
 	public ControladorQueries(CtrlGraf CtrlGraf){
 		this.CtrlGraf = CtrlGraf;
 		HS = new HeteSim(CtrlGraf.consultarGraf());
 		UltimesQueries = new ArrayList<Query>();
 	}
 	
+	/**
+	 * Inicialitza una query de tipus clustering
+	 * @param cami Camí sobre el que fer la query
+	 * @param k Número de clústers
+	 * @throws Exception 
+	 */
 	public void inicialitzarQueryClustering(String cami, int k) throws Exception{
 		Cami c = new Cami("nom", cami, "descr");
 		QueryActual = new QueryClustering(c,k,HS);
 	}
 	
+	/**
+	 * Inicialitza una query de tipus normal
+	 * @param cami Camí sobre el que fer la query
+	 * @throws Exception
+	 */
 	public void inicialitzarQuerynormal(String cami) throws Exception{
 		Cami c = new Cami("nom", cami, "descr");
 		QueryActual = new QueryNormal(c);
 	}
 	
+	/**
+	 * Selecciona l'entitat inicial de la query
+	 * @param nom Nom de l'entitat inicial a afegir
+	 * @throws Exception No es pot afegir una entitat inicial a una query de tipus clustering
+	 */
 	public void seleccionarEntitatInicial(String nom) throws Exception{
-		if(QueryActual instanceof QueryNormal){
+		if(QueryActual.esNormal()){
 			String tipus = QueryActual.getPath().substring(0,1);
 			
 			tipus = normalitzarTipus(tipus);
@@ -47,8 +72,13 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Selecciona l'entitat final de la query
+	 * @param nom Nom de l'entitat final a afegir
+	 * @throws Exception No es pot afegir una entitat final a una query de tipus clustering
+	 */
 	public void seleccionarEntitatFinal(String nom) throws Exception{
-		if(QueryActual instanceof QueryNormal){
+		if(QueryActual.esNormal()){
 			String tipus = QueryActual.getPath();
 			tipus = tipus.substring(tipus.length()-1, tipus.length());
 			
@@ -67,8 +97,13 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Executa una query de tipus clustering 
+	 * @return Matriu d'Strings que són clústers d'entitats
+	 * @throws Exception La query no és de tipus clustering
+	 */
 	public ArrayList<ArrayList<String>> executarClustering() throws Exception{
-		if(QueryActual instanceof QueryClustering){
+		if(QueryActual.esClustering()){
 			
 			ArrayList<ArrayList<Entitat>> resultatClustering = ((QueryClustering) QueryActual).getResultatClusters(CtrlGraf.consultarGraf());
 			ArrayList<ArrayList<String>> resultat = new ArrayList<ArrayList<String>>();
@@ -87,8 +122,13 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Executa una query de tipus normal
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception La query no és de tipus normal
+	 */
 	public ArrayList<Pair<Entitat, Float>> executarQuery() throws Exception{ // String -> Integer
-		if(QueryActual instanceof QueryNormal){
+		if(QueryActual.esNormal()){
 			ArrayList<Pair<Integer, Float>> resultat = ((QueryNormal) QueryActual).executa(HS);
 			ArrayList<Pair<Entitat, Float>> r = new ArrayList<Pair<Entitat,Float>>();
 		
@@ -124,9 +164,17 @@ public class ControladorQueries{
 		}
 	}
 	
-	public ArrayList<Pair<Entitat, Float>> filtrarResultatN(String n) throws Exception{
+	/**
+	 * Filtra el resultat actual segons el número d'entitats a mostrar
+	 * @param n Número d'entitats que es vol que es mostrin
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public ArrayList<Pair<Entitat, Float>> filtrarResultatN(int n) throws Exception{
 		if(ResultatActual != null){
-			ResultatActual.filtrarN(Integer.valueOf(n));
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
+			ResultatActual.filtrarN(n);
 			ArrayList<Pair<Entitat, Float>> entitats = ResultatActual.getVisibles();
 			
 			return entitats;
@@ -136,8 +184,16 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Filtrar el resultat actual segons la label de les entitats a mostrar
+	 * @param label Label de les entitats que es volen mostrar
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
 	public ArrayList<Pair<Entitat, Float>> filtrarResultatLabel(int label) throws Exception{
 		if(ResultatActual != null){
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
 			ResultatActual.filtrarLabelEq(label);
 			ArrayList<Pair<Entitat, Float>> entitats = ResultatActual.getVisibles();
 			
@@ -148,8 +204,16 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Filtra el resultat actual segons la label de les entitats que no es volen mostrar
+	 * @param label Label de les entitats que no es volen mostrar
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
 	public ArrayList<Pair<Entitat, Float>> filtrarResultatNoLabel(int label) throws Exception{
 		if(ResultatActual != null){
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
 			ResultatActual.filtrarLabelDif(label);
 			ArrayList<Pair<Entitat, Float>> entitats = ResultatActual.getVisibles();
 			
@@ -160,7 +224,139 @@ public class ControladorQueries{
 		}
 	}
 	
-	public String[] consultarQueryRecent(int i){
+	/**
+	 * Neteja els filtres del resultat
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public ArrayList<Pair<Entitat, Float>> netejaFiltresResultat() throws Exception{
+		if(ResultatActual != null){
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
+			ResultatActual.netejaFiltres();
+			ArrayList<Pair<Entitat, Float>> entitats = ResultatActual.getVisibles();
+			
+			return entitats;
+		}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+	
+	/**
+	 * Filtra el resultat segons l'entitat que es vol ocultar
+	 * @param id Id de l'entitat que es vol ocultar
+	 * @return Llista de parelles d'Entitat i valor d'HeteSim que formen el resultat a mostrar
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public ArrayList<Pair<Entitat,Float>> filtrarResultatEntitat(int id) throws Exception{
+		if(ResultatActual != null){
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
+			ResultatActual.filtrarEntitat(id);
+			ArrayList<Pair<Entitat, Float>> entitats = ResultatActual.getVisibles();
+			
+			return entitats;
+		}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+
+	/**
+	 * Elimina l'ultim filtre activat
+	 * @throws Exception El resultat actual és d'una query de clustering
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public void resultatDesferFiltre() throws Exception{
+		if(ResultatActual != null){
+			if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
+			ResultatActual.desferFiltre();
+		}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	 }
+	    
+	/**
+	 * Comprova si el resultat actual està format per autors
+	 * @return True si el resultat són autors, false en cas contrari
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public boolean resultatIsAutor() throws Exception {
+		if(ResultatActual != null){
+			if(QueryActual.esNormal()) return ResultatActual.isAutor();
+			else{
+				return (QueryActual.getPath().charAt(0)=='A');
+			}
+		}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+	    
+	/**
+	 * Comprova si el resultat actual està format per papers
+	 * @return True si el resultat són papers, false en cas contrari
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public boolean resultatIsPaper() throws Exception{
+		if(ResultatActual != null){
+			if(QueryActual.esNormal()) return ResultatActual.isPaper();
+			else{
+				return (QueryActual.getPath().charAt(0)=='P');
+			}
+		}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+	    
+	/**
+	 * Comprova si el resultat actual està format per conferencies
+	 * @return True si el resultat són conferencies, false en cas contrari
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public boolean resultatIsConferencia() throws Exception{
+    	if(ResultatActual != null){
+    		if(QueryActual.esNormal()) return ResultatActual.isConferencia();
+			else{
+				return (QueryActual.getPath().charAt(0)=='C');
+			}
+    	}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+	    
+	/**
+	 * Comprova si el resultat actual està format per papers
+	 * @return True si el resultat són papers, false en cas contrari
+	 * @throws Exception No hi ha resultat actual
+	 */
+	public boolean resultatIsTerme() throws Exception{
+    	if(ResultatActual != null){
+    		if(QueryActual.esNormal()) return ResultatActual.isTerme();
+			else{
+				return (QueryActual.getPath().charAt(0)=='T');
+			}
+    	}
+		else{
+			throw new Exception("No hi ha resultat");
+		}
+	}
+	
+	/**
+	 * Consulta una query recent. Es guarden les 3 queries més recents
+	 * @param i Número de la query recent a consultar (0 és la query més recent)
+	 * @return Vector de Strings amb les característiques de la query consultada
+	 * @throws Exception No hi ha queries recents
+	 * @throws Exception i>=3
+	 */
+	public String[] consultarQueryRecent(int i) throws Exception{
+		if(UltimesQueries.size()==0) throw new Exception("No hi ha queries recents");
+		if(i>=3) throw new Exception("L'index supera el número de queries guardades");
+		
 		Query c = UltimesQueries.get(i);
 		String[] query;
 		if(c.esClustering()){
@@ -178,7 +374,14 @@ public class ControladorQueries{
 		return query;
 	}
 	
-	public String[] consultarQueryActual(){
+	/**
+	 * Consulta la query actual
+	 * @return Vector de Strings amb les característiques de la query actual
+	 * @throws Exception No hi ha query actual
+	 */
+	public String[] consultarQueryActual() throws Exception{
+		if(QueryActual==null) throw new Exception("No hi ha query actual");
+		
 		String[] query;
 		if(QueryActual.esClustering()){
 			query = new String[2];
@@ -195,7 +398,13 @@ public class ControladorQueries{
 		return query;
 	}
 	
-	private void afegirQueryRecent(){
+	/**
+	 * Afegeix una query a les queries recents
+	 * @throws Exception No hi ha query actual
+	 */
+	private void afegirQueryRecent() throws Exception{
+		if(QueryActual==null) throw new Exception("No hi ha query actual");
+		
 		if(UltimesQueries.size()==3){
 			UltimesQueries.remove(2);
 			UltimesQueries.add(0, QueryActual);
@@ -205,6 +414,11 @@ public class ControladorQueries{
 		}
 	}
 	
+	/**
+	 * Normalitza el tipus passant de la inicial a l'String complet
+	 * @param tipus String amb el tipus desitjat al primer caràcter.
+	 * @return Tipus normalitzat
+	 */
 	private String normalitzarTipus(String tipus){
 		if(tipus.charAt(0)=='A') tipus = "Autor";
 		else if(tipus.charAt(0)=='C') tipus = "Conferencia";
@@ -214,5 +428,17 @@ public class ControladorQueries{
 		return tipus;
 	}
 	
+	/**
+	 * Consulta les dades del resultat actual d'una query normal
+	 * @return Matriu d'Strings amb les dades del resultat
+	 * @throws Exception No hi ha resultat actual
+	 * @throws Exception La query actual és de clustering
+	 */
+    public String[][] getDadesNormal() throws Exception{
+    	if(ResultatActual==null) throw new Exception("No hi ha resultat actual");
+    	if(QueryActual.esClustering()) throw new Exception("La query actual és de clustering");
+    	
+    	return ResultatActual.getDadesString();
+    }
 	
 }
