563f7903cb3c39cf0f4c9530d80864d872362935
marc-villanueva-cuadrench <marcvillanuevacuadrench@gmail.com>
2016-05-29 23:50:55 +0200

    Update QueryNormal.java

--- src/domini/queries/QueryNormal.java
+++ src/domini/queries/QueryNormal.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import domini.camins.Cami;
 import domini.graf.Entitat;
 
+/**
+ * La classe QueryNormal és una subclasse de la classe Query per les queries de tipus normal (no clustering)
+ * @author Marc Villanueva Cuadrench
+ *
+ */
 public class QueryNormal extends Query{
 	private String cami;
 	private Entitat EntitatInicial;
@@ -11,41 +16,76 @@ public class QueryNormal extends Query{
 	private Entitat EntitatNoInicial;
 	private int posEF;
 
-	
+	/**
+	 * Crea una instancia de QueryNormal amb el camí passat com a paràmetre
+	 * @param c
+	 * @throws Exception
+	 */
 	public QueryNormal(Cami c) throws Exception{
+		super.cami = c;
 		cami = c.getPath();
 	}
 	
+	/**
+	 * Retorna false ja que no és una query de clustering
+	 */
 	public boolean esClustering(){
 		return false;
 	}
 	
+	/**
+	 * Retorna true ja que és una query normal
+	 */
 	public boolean esNormal(){
 		return true;
 	}
 	
+	/**
+	 * Consultora del camí de la query
+	 * @return Retorna un String amb el path del camí
+	 */
 	public String getPath(){
 		return cami;
 	}
 	
-	
+	/**
+	 * Consultora de la posició al graf de l'entitat inicial
+	 * @return int amb la posició de la matriu del graf on es troba l'entitat inicial
+	 */
 	public int getEI(){
 		return posEI;
 	}
 	
+	/**
+	 * Consultora de la posició al graf de l'entitat final
+	 * @return int amb la posició de la matriu del graf on es troba l'entitat final
+	 */
 	public int getEF(){
 		return posEF;
 	}
 	
+	/**
+	 * Consultora de l'entitat inicial de la query
+	 * @return Objecte de tipus Entitat
+	 */
 	public Entitat getEntitatInicial(){
 		return EntitatInicial;
 	}
 	
+	/**
+	 * Consultora de l'entitat final de la query
+	 * @return Objecte de tipus Entitat
+	 */
 	public Entitat getEntitatNoInicial(){
 		return EntitatNoInicial;
 	}
 	
-	
+	/**
+	 * Guarda l'entitat inicial de la query
+	 * @param e Entitat que es guardarà
+	 * @param pos Posició de l'entitat a la matriu del graf
+	 * @throws Exception El tipus de l'entitat no concorda amb el camí
+	 */
 	public void setEntitatInicial(Entitat e, int pos) throws Exception{
 		char c = cami.charAt(0);
 		if(c=='A'){
@@ -67,7 +107,12 @@ public class QueryNormal extends Query{
 	}
 	
 	
-	
+	/**
+	 * Guarda l'entitat final de la query
+	 * @param e Entitat que es guardarà
+	 * @param pos Posició de l'entitat a la matriu del graf
+	 * @throws Exception El tipus de l'entitat no concorda amb el camí
+	 */
 	public void setEntitatNoInicial(Entitat e, int pos) throws Exception{
 			int i = cami.length()-1;
 			char c = cami.charAt(i);
@@ -89,8 +134,12 @@ public class QueryNormal extends Query{
 			}
 	}
 
-	
-	
+	/**
+	 * Executa la query
+	 * @param HS Objecte de tipus HeteSim que s'utilitza per calcular el resultat
+	 * @return Vector d'entitats amb els seus valors d'HeteSim que representa el resultat de la query
+	 * @throws Exception Falta algun paràmetre de la query
+	 */
 	public ArrayList<Pair<Integer, Float>> executa(HeteSim HS) throws Exception{
 		if(cami!=null){
 			if(EntitatInicial != null){
