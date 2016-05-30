faebf4b04fc5d822ca0604822063d5c42d5a4634
Martiruhay <marti-nini@hotmail.com>
2016-05-30 10:42:59 +0200

    Add files via upload

---
+++ src/presentacio/VistaQueriesRecents.java
@@ -0,0 +1,111 @@
+package presentacio;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTree;
+import javax.swing.table.AbstractTableModel;
+
+public class VistaQueriesRecents {
+
+	private JFrame frame;
+	private JTable table;
+	private CtrlPresentacio ctrl;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaQueriesRecents window = new VistaQueriesRecents(new CtrlPresentacio());
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VistaQueriesRecents(CtrlPresentacio ctrl) {
+		this.ctrl = ctrl;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[grow][][][][][][][][][][][][]", "[][grow]"));
+		
+		JButton btnEnrere = new JButton("Enrere");
+		btnEnrere.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+			}
+		});
+		frame.getContentPane().add(btnEnrere, "cell 0 0");
+		
+		JButton btnExecutar = new JButton("Obrir");
+		btnExecutar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnExecutar, "cell 12 0");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, "cell 0 1 12 1,grow");
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+	}
+	
+	private class RecQueTableModel extends AbstractTableModel {
+		private static final long serialVersionUID = 1L;
+		protected String[] columnNames = {"Entitat", "Label", "Hetesim"};
+		protected String[][] data;
+		
+		public RecQueTableModel() {
+			
+		}
+		
+		public String getColumnName(int column) {
+			return columnNames[column];
+		}
+		
+		public boolean isCellEditable(int row, int column) {
+			return false;
+		}
+
+		public int getRowCount() {
+			return data.length;
+		}
+		
+		public void updateData() {
+			
+			fireTableDataChanged();
+		}
+
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		public String getValueAt(int fila, int columna) {
+			return data[fila][columna];
+		}
+	}
+
+}
