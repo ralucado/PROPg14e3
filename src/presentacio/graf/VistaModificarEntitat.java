51b81228651c0db489ee2df61893c24ca41dac71
marc-villanueva-cuadrench <marcvillanuevacuadrench@gmail.com>
2016-05-31 22:17:39 +0200

    Update VistaModificarEntitat.java

--- src/presentacio/graf/VistaModificarEntitat.java
+++ src/presentacio/graf/VistaModificarEntitat.java
@@ -1,30 +1,38 @@
 package presentacio.graf;
+	import java.awt.BorderLayout;
 	import java.awt.Color;
-import java.awt.SystemColor;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+	import java.awt.FlowLayout;
+	import java.awt.SystemColor;
+	import java.awt.TextField;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+	import javax.swing.JButton;
+	import javax.swing.JDialog;
+	import javax.swing.JFrame;
+	import javax.swing.JPanel;
+	import javax.swing.border.EmptyBorder;
 
-import net.miginfocom.swing.MigLayout;
+import domini.graf.Autor;
+import domini.graf.Label;
+	import net.miginfocom.swing.MigLayout;
 import presentacio.ctrl.CtrlPresentacio;
 import presentacio.ctrl.VistaDialog;
+
+import javax.swing.JComboBox;
+	import java.awt.event.ActionListener;
+	import java.awt.event.ItemEvent;
+	import java.awt.event.ItemListener;
+	import java.awt.event.ActionEvent;
+	import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 	public class VistaModificarEntitat extends JDialog {
 
 		private JTextField textField;
-		private JComboBox comboBox_1;
-		private JComboBox comboBox_1_1;
+		private JComboBox comboBox_1_1_1;
 		private CtrlPresentacio ctrl;
-		private String[] labels = {"", "UNKNOWN", "DATABASE", "DATA_MINING", "AI", "INFORMATION_RETRIEVAL"};
+		private String[] labels = {"UNKNOWN", "DATABASE", "DATA_MINING", "AI", "INFORMATION_RETRIEVAL"};
 		
 		//Entitat
 		private String nomAct;
@@ -33,6 +41,11 @@ import presentacio.ctrl.VistaDialog;
 		private String label;
 
 		private void initComponents(){
+			
+			try{
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+			}
+			catch(Exception e){}
 			//frame
 			
 			setBackground(SystemColor.control);
@@ -64,55 +77,8 @@ import presentacio.ctrl.VistaDialog;
 			lblLabel.setBounds(255, 10, 136, 15);
 			getContentPane().add(lblLabel, "cell 0 6,alignx left,aligny center");
 			
-				
-			//boto modificar
-			JButton okButton = new JButton("Modificar entitat");
-			okButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if(label==null || label == ""){
-						if(nom != null){
-							
-							try {
-								ctrl.getDomini().modificarEntitat(nomAct, nom, null, tipus);
-								System.out.println("S'ha modificat el nom a: "+nom);
-							} catch (Exception e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						else{
-							VistaDialog d = new VistaDialog();
-							d.setDialog("Error", "Afegeix algun camp a modificar",new String[]{"Acceptar"}, JOptionPane.ERROR_MESSAGE);
-						}
-					}
-					else{
-						if(nom != null){
-							try{
-								
-								ctrl.getDomini().modificarEntitat(nomAct, nom, label, tipus);
-								System.out.println("S'ha modificat el nom a :"+nom+" i la label a: "+label);
-							}
-							catch(Exception e2){
-								e2.printStackTrace();
-							}
-						}
-						else{
-							try{
-								System.out.println("S'ha modificat la label a: "+label);
-								ctrl.getDomini().modificarEntitat(nomAct, null, label, tipus);
-							}
-							catch(Exception e3){
-								e3.printStackTrace();
-							}
-						}
-					}
-					dispose();
-					
-				}
-			});
-			
 			//seleció label
-			JComboBox comboBox_1_1_1 = new JComboBox(labels);
+			comboBox_1_1_1 = new JComboBox(labels);
 			comboBox_1_1_1.setBounds(255, 37, 113, 24);
 			getContentPane().add(comboBox_1_1_1, "cell 3 6,alignx left,aligny top");
 			
@@ -124,6 +90,26 @@ import presentacio.ctrl.VistaDialog;
 				}
 			});
 			comboBox_1_1_1.setSelectedItem(label);
+				
+			//boto modificar
+			JButton okButton = new JButton("Modificar entitat");
+			okButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						nom = textField.getText();
+						if(nom.equals("")) throw new Exception("El nom no pot estar buit");
+						label = (String) comboBox_1_1_1.getSelectedItem();
+						ctrl.getDomini().modificarEntitat(nomAct, nom, label, tipus);
+						dispose();
+					} catch (Exception e1) {
+						VistaDialog d = new VistaDialog();
+						d.setDialog("Error", e1.getMessage(),new String[]{"Acceptar"}, JOptionPane.ERROR_MESSAGE);
+					}
+					
+				}
+			});
+			
+			
 			
 			getContentPane().add(okButton, "cell 0 9");
 			
@@ -148,7 +134,8 @@ import presentacio.ctrl.VistaDialog;
 			this.nomAct = nomAct;
 			this.tipus = tipus;
 			this.label = label;
-			initComponents();		
+			initComponents();
+			if(tipus=="Terme") comboBox_1_1_1.setEnabled(false);
 			setVisible(true);
 			setLocationRelativeTo(null);
 		}
